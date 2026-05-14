88190c7addfb68e89a420bb5f48fca0349430b13
DegraNkoxer <131817969+DegraNkoxer@users.noreply.github.com>
2026-05-14 08:10:48 -0700

    Update and rename Android.mk to AndroidProducts.mk

--- Android.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),k53_m9_32g)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_k53_m9_32g.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_k53_m9_32g-eng \
+    omni_k53_m9_32g-user
